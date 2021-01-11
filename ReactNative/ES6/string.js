f7535591c517e5ad08b54ba63742d80ecc9320ad
sieun.lee <sieun.lee@voithru.com>
2021-01-12 01:43:31 +0900

    chore: add comments

--- ReactNative/ES6/string.js
+++ ReactNative/ES6/string.js
@@ -7,6 +7,7 @@ const val3 = val1 + ' ' + val2 + '!'
 console.log(val3)
 
 // Literal: `${variable}`
+// python의 format같이 여러 문자열을 편하게 쓸 수 있게 해주는 역할
 const litval = `${val1} ${val2}~!~!`
 
 console.log(litval)
