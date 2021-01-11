76ec51156239d99a9d71a24239fb4aa13c1bb063
sieun.lee <sieun.lee@voithru.com>
2021-01-12 00:37:12 +0900

    es6 study: stringLiteral

---
+++ ReactNative/ES6/string.js
@@ -0,0 +1,12 @@
+//String Literal
+// String
+const val1  ='hello';
+const val2 = 'world';
+const val3 = val1 + ' ' + val2 + '!'
+
+console.log(val3)
+
+// Literal: `${variable}`
+const litval = `${val1} ${val2}~!~!`
+
+console.log(litval)
