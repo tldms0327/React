7bf5cf2a362feb1fa084f9014fb91af6c034d546
Sieun.Lee <tldms0327@yonsei.ac.kr>
2021-01-16 02:55:55 +0900

    feat: get weather using api and location

--- ReactNative/weather-app/App.js
+++ ReactNative/weather-app/App.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from './Loading'
+import Weather from './Weather';
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-      <Loading/>
-  );
+const API_key = "05b42e753150dfd47707b0ab43139c5f";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async(latitude, longitude) => {
+    const { 
+      data: {main: {temp}, weather} 
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_key}&units=metric`
+    );
+    
+    this.setState({ 
+      isLoading: false, condition: weather[0].main, temp
+    })
+    console.log(this.state)
+
+  };
+
+  getLocation = async() => {
+    try{
+      //위치 접근 권한 받아오기
+      await Location.requestPermissionsAsync(); // 이 값을 변수에 넣고 console에 찍어보면 내용을 알 수 있다
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync(); // 필요한 정보만 가져오자
+      console.log(latitude, longitude);  //위 정보를 api로 던져서 날씨를 받아올거야
+
+      this.getWeather(latitude, longitude);
+
+  } catch(error) {
+    //try 블럭에서 에러가 났을 때는 경고하기
+    Alert.alert("something going wrong...", "ㅠ.ㅠ")
+  }
+  };
+
+  componentDidMount() {
+    this.getLocation()
+  };
+
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/>  
+  }
+
 }
 
 
---
+++ ReactNative/weather-app/Weather.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+
+export default function Weather({temp}, {condition}) {
+    return (
+    <View style={styles.container}>
+        <Text>
+            {condition}
+        </Text>
+        <Text>
+            {condition}
+        </Text>
+    </View>);
+
+}
+
+Weather.propTypes = {
+    temp:PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze"
+    ]).isRequired
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+}
+    
+)
